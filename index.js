f8a47c19946e2acb52251de252cb1e26a36b04be
Numbers00 <kylemariangelo@gmail.com>
2023-09-02 11:04:21 +0800

    Attempt to connect to postgresql db in render

--- index.js
+++ index.js
@@ -9,7 +9,14 @@ require('dotenv').config();
 
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DATABASE_URL);
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  },
+});
 
 const main = async () => {
   try {
