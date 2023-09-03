e0a866b6a5ce98b0d420e45691c1a9b8a24875cb
Numbers00 <kylemariangelo@gmail.com>
2023-09-03 17:32:30 +0800

    Upload for submission up to exer 13.22

--- controllers/readinglists.js
+++ controllers/readinglists.js
@@ -2,6 +2,8 @@ const router = require('express').Router();
 
 const { Blog, ReadingList, User } = require('../models');
 
+const { tokenExtractor } = require('../utils/middleware');
+
 router.post('/', async (req, res) => {
   const { body } = req;
 
@@ -12,6 +14,23 @@ router.post('/', async (req, res) => {
   if (!user) return res.status(404).end();
 
   const readingList = await ReadingList.create({ ...body, blogId: blog.id, userId: user.id });
+  res.json(readingList);
+});
+
+router.put('/:id', tokenExtractor, async (req, res) => {
+  const { body: { wasRead } } = req;
+
+  const user = await User.findByPk(req.decodedToken.id);
+  if (!user) return res.status(401).end();
+
+  const readingList = await ReadingList.findByPk(req.params.id);
+  if (!readingList) return res.status(404).end();
+
+  if (user.id !== readingList.userId) return res.status(401).end();
+
+  readingList.wasRead = wasRead === 'true' || wasRead === true;
+  await readingList.save();
+
   res.json(readingList);
 });
 
